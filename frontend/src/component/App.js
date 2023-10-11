407ccb68e011815fafb2ddadedf2fcb419074c89
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-11 10:03:37 +0530

    working on header

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -5,7 +5,7 @@ import Header from './Header'
 function App(){
     return (
         <div>
-            <Header pageTitle="Frontend authenticated with JWT" logoSrc={logo} />
+            <Header pageTitle="Frontend authenticated with JWT" className='App-logo' logoSrc={logo} />
         </div>
     )
 }
