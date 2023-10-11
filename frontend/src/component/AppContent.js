f655d6e161722b23f16112062992ae14bd70a7ae
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-11 10:34:22 +0530

    create welcome content component

--- frontend/src/component/AppContent.js
+++ frontend/src/component/AppContent.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import WelComeContent from './WelcomeContent';
 
 export default class AppContent extends React.Component {
     render(){
         return(
             <div>
-                
+                <WelComeContent />
             </div>
         )
     }
